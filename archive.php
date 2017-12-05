5bdff840ccc60401e2e42e28ca5932d3dc5be449
alxhar <haritonov.aka@gmail.com>
2017-12-06 02:20:59 +0300

    fix archive page

--- archive.php
+++ archive.php
@@ -15,34 +15,33 @@
 the_archive_title( '<h1 class="uk-heading-line uk-text-center"><span>', '</span></h1>' );
 the_archive_description( '<div class="taxonomy-description">', '</div>' );
 ?>
-<?php if ( have_posts() ) : ?>
     <div class="uk-grid uk-grid-match uk-grid-small" uk-grid>
-	<?php while ( have_posts() ) :
-		the_post();
-		get_template_part( 'template-parts/page/content', 'front-page' );
-		?>
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/page/content', 'front-page' );
+				?>
 
-	<?php endwhile; ?>
-    <div class="uk-width-1-1">
-		<?php astro_pagination(); ?>
-    </div>
-<?php else: ?>
-    <div class="uk-width-1-1">
-        <div class="uk-flex uk-flex-center">
-            <div class="uk-card uk-card-body">
-                <div class="uk-card-media-top">
-                    <img src="<?php echo get_site_icon_url( 192 ); ?>" class="uk-align-center">
-                </div>
-                <p class="uk-heading-primary uk-text-center"><span>Пока еще нет опбубликованых записей</span></p>
-                <div class="uk-text-center">
-                    <a href="<?php echo site_url(); ?>">На главную</a>
+			<?php endwhile; ?>
+            <div class="uk-width-1-1">
+				<?php astro_pagination(); ?>
+            </div>
+		<?php else: ?>
+            <div class="uk-width-1-1">
+                <div class="uk-flex uk-flex-center">
+                    <div class="uk-card uk-card-body">
+                        <div class="uk-card-media-top">
+                            <img src="<?php echo get_site_icon_url( 192 ); ?>" class="uk-align-center">
+                        </div>
+                        <p class="uk-heading-primary uk-text-center"><span>Пока еще нет опбубликованых записей</span>
+                        </p>
+                        <div class="uk-text-center">
+                            <a href="<?php echo site_url(); ?>">На главную</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+		<?php endif; ?>
     </div>
-    </div>
-
-
-<?php endif; ?>
 
 <?php get_footer(); ?>
